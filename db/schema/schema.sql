98deb106a1ba6be8386b224249b9ee26ef3364f4
Brandon Griffith <BrandonGri5th@gmail.com>
2018-08-01 18:18:54 -0600

    day1

---
+++ db/schema/schema.sql
@@ -0,0 +1,28 @@
+Table
+SQL
+create table MOCK_DATA (
+	id SERIAL PRIMARY KEY,
+	product_name VARCHAR(50),
+	product_price VARCHAR(50),
+	product_image VARCHAR(50)
+);
+insert into MOCK_DATA (id, product_name, product_price, product_image) values (1, 'Crab - Claws, Snow 16 - 24', '$5.26', 'http://dummyimage.com/249x177.jpg/5fa2dd/ffffff');
+insert into MOCK_DATA (id, product_name, product_price, product_image) values (2, 'Veal - Osso Bucco', '$8.55', 'http://dummyimage.com/236x214.png/ff4444/ffffff');
+insert into MOCK_DATA (id, product_name, product_price, product_image) values (3, 'Longos - Chicken Wings', '$4.11', 'http://dummyimage.com/135x104.bmp/cc0000/ffffff');
+insert into MOCK_DATA (id, product_name, product_price, product_image) values (4, 'Cheese - Gouda Smoked', '$8.70', 'http://dummyimage.com/246x176.png/dddddd/000000');
+insert into MOCK_DATA (id, product_name, product_price, product_image) values (5, 'Red Cod Fillets - 225g', '$1.92', 'http://dummyimage.com/121x204.jpg/dddddd/000000');
+insert into MOCK_DATA (id, product_name, product_price, product_image) values (6, 'Veal - Bones', '$1.75', 'http://dummyimage.com/219x184.bmp/dddddd/000000');
+insert into MOCK_DATA (id, product_name, product_price, product_image) values (7, 'Cookies - Englishbay Oatmeal', '$7.64', 'http://dummyimage.com/228x130.bmp/ff4444/ffffff');
+insert into MOCK_DATA (id, product_name, product_price, product_image) values (8, 'Muffin Puck Ww Carrot', '$3.19', 'http://dummyimage.com/103x108.png/ff4444/ffffff');
+insert into MOCK_DATA (id, product_name, product_price, product_image) values (9, 'Tuna - Salad Premix', '$2.64', 'http://dummyimage.com/238x106.bmp/dddddd/000000');
+insert into MOCK_DATA (id, product_name, product_price, product_image) values (10, 'Soup - Knorr, Veg / Beef', '$5.89', 'http://dummyimage.com/102x132.bmp/ff4444/ffffff');
+insert into MOCK_DATA (id, product_name, product_price, product_image) values (11, 'Nantucket Pine Orangebanana', '$7.55', 'http://dummyimage.com/142x166.png/5fa2dd/ffffff');
+insert into MOCK_DATA (id, product_name, product_price, product_image) values (12, 'Bok Choy - Baby', '$1.74', 'http://dummyimage.com/147x168.jpg/dddddd/000000');
+insert into MOCK_DATA (id, product_name, product_price, product_image) values (13, 'Wine - Red Oakridge Merlot', '$8.37', 'http://dummyimage.com/149x143.bmp/ff4444/ffffff');
+insert into MOCK_DATA (id, product_name, product_price, product_image) values (14, 'Water Chestnut - Canned', '$7.08', 'http://dummyimage.com/228x172.jpg/cc0000/ffffff');
+insert into MOCK_DATA (id, product_name, product_price, product_image) values (15, 'Wine - Barbera Alba Doc 2001', '$3.77', 'http://dummyimage.com/179x128.bmp/dddddd/000000');
+insert into MOCK_DATA (id, product_name, product_price, product_image) values (16, 'Basil - Dry, Rubbed', '$7.72', 'http://dummyimage.com/241x171.bmp/ff4444/ffffff');
+insert into MOCK_DATA (id, product_name, product_price, product_image) values (17, 'Milk - Chocolate 500ml', '$9.21', 'http://dummyimage.com/118x205.png/ff4444/ffffff');
+insert into MOCK_DATA (id, product_name, product_price, product_image) values (18, 'Lettuce - Radicchio', '$5.35', 'http://dummyimage.com/127x245.jpg/ff4444/ffffff');
+insert into MOCK_DATA (id, product_name, product_price, product_image) values (19, 'Island Oasis - Lemonade', '$4.21', 'http://dummyimage.com/173x147.png/dddddd/000000');
+insert into MOCK_DATA (id, product_name, product_price, product_image) values (20, 'Chocolate Liqueur - Godet White', '$7.08', 'http://dummyimage.com/113x195.jpg/cc0000/ffffff');
